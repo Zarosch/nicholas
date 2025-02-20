b895ba7af3d6e97eaf09188fcfa200cdd5c93d7c
Zarosch <kevin@velz.dev>
2025-02-20 20:00:00 +0100

    Encode Tag url by display_tag_list

--- themes/default/functions.php
+++ themes/default/functions.php
@@ -4,7 +4,7 @@ function display_tag_list($tags) {
 	$output = '';
 	
 	foreach ($tags as $tag) {
-		$output .= 	'<a href="' . $GLOBALS['config']['base_url'] . '/tag/' . strtolower($tag) . '/" title="Posts tagged ' . $tag . '">' . $tag . '</a>, ';
+		$output .= 	'<a href="' . $GLOBALS['config']['base_url'] . '/tag/' . urlencode(strtolower($tag)) . '/" title="Posts tagged ' . $tag . '">' . $tag . '</a>, ';
 	}
 	
 	$output = rtrim($output, ', ');
