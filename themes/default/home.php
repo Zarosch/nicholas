b1f8368916e9fed36341c3c8b4e47c54a17586e7
Zarosch <kevin@velz.dev>
2025-02-20 21:16:02 +0100

    correct code ident

--- themes/default/home.php
+++ themes/default/home.php
@@ -2,15 +2,13 @@
 
 		
 <?php foreach ($posts as $post) { ?>		
-
-<article class="blog-preview">
-	<a href="<?= get_post_link($post); ?>">
-		<h2><?= $post->title; ?></h2>
-	</a>
-	<p><?= $post->excerpt; ?></p>
-	<p class="small"><?= date($GLOBALS['config']['date_format'], $post->date); ?> • Filed under <?= display_tag_list($post->tags); ?></p>
-</article>
-
+	<article class="blog-preview">
+		<a href="<?= get_post_link($post); ?>">
+			<h2><?= $post->title; ?></h2>
+		</a>
+		<p><?= $post->excerpt; ?></p>
+		<p class="small"><?= date($GLOBALS['config']['date_format'], $post->date); ?> • Filed under <?= display_tag_list($post->tags); ?></p>
+	</article>
 <?php } ?>
 
 <div class="pagination">
